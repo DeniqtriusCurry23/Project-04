b1c71ce9746bf9ff2f21a83847e1409dd7e83d4b
Deniqtrius Curry <Deniqtrius.curry@outlook.com>
2018-06-06 12:33:34 -0700

    Asp.Net Dream Connect Commit

---
+++ DreamConnectttt/Extensions/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DreamConnectttt.Models;
+
+namespace DreamConnectttt.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "We here at Dream Connect have strong beliefs that dreams are a gateway to an alternate reality.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Feel free to reach out to us here at Dream Connect";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DreamConnectttt/Models/MessageModel.cs
@@ -0,0 +1,6 @@
+﻿namespace DreamConnectttt.Controllers
+{
+    public class MessageModel
+    {
+    }
+}
